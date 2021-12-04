8919dd7029e4116ed696699d9cfda38934f20dda
rash-123 <vermarashmi559@gmail.com>
2021-12-04 18:29:25 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import SignUp from "./SignUp"
+import { Container } from 'react-bootstrap'
+import { AuthProvider } from "../contexts/AuthContext";
+
+function App() {
+  return (
+  <AuthProvider>
+    <Container className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: '400px'}}>
+        <SignUp />
+      </div>
+    </Container>
+  </AuthProvider>
+  )
+}
+
+export default App;
